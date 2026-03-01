e7d1e4a306ef28cf55c7aefcc922759c308ba593
Kenys Padilla <kvpadilla@unicesar.edu.co>
2026-03-01 18:49:50 -0500

    feat: crear clase Asignatura con atributos

---
+++ src/sistemaacademicov1/Asignatura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademicov1;
+
+/**
+ *
+ * @author vanes
+ */
+public class Asignatura {
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
+}

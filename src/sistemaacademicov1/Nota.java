195675264864bffd148395eb2243f3e78898a6b9
Kenys Padilla <kvpadilla@unicesar.edu.co>
2026-03-01 18:57:53 -0500

    feat: crear clase Nota con atributos

---
+++ src/sistemaacademicov1/Nota.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademicov1;
+
+/**
+ *
+ * @author vanes
+ */
+public class Nota {
+    private Estudiante estudiante;
+    private Asignatura asignatura;
+    private double valor;
+    private String periodo;
+}
